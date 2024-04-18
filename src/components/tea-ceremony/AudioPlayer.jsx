318fe874f481fa001843d3a3724583d593682670
Gregory Cowley <gregory@cowleystudios.com>
2024-04-18 08:16:24 -0700

    package.json update

---
+++ src/components/tea-ceremony/AudioPlayer.jsx
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from 'react';
+
+function AudioPlayer() {
+  // Reference to the audio element
+  const audioRef = useRef(null);
+  
+  // State to manage whether the audio should loop
+  const [loop, setLoop] = useState(false);
+
+  // Function to play the audio
+  const playAudio = () => {
+    audioRef.current.play();
+  };
+
+  // Function to toggle looping
+  const toggleLoop = () => {
+    setLoop(!loop);
+  };
+
+  return (
+    <div>
+      <audio ref={audioRef} src="sample-audio.mp3" loop={loop} />
+      <button onClick={playAudio}>Play Audio</button>
+      <div>
+        <label>
+          <input type="checkbox" checked={loop} onChange={toggleLoop} />
+          Loop playback
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default AudioPlayer;
