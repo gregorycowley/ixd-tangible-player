36d18f4207a23781d6117258d6e171cb7e21b78f
Gregory Cowley <gregory@cowleystudios.com>
2024-04-21 08:34:48 -0700

    Merge complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React, { StrictMode } from 'react';
-<<<<<<< HEAD
-import TeaCeremony from './components/tea-ceremony/TeaCeremony.jsx';
-import Puck from './components/puck-proxies/puck/Puck.jsx';
-=======
 import AppContainer from './containers/AppContainer.jsx';
->>>>>>> et-operation
 
 function App() {
   return (
     <StrictMode>
-<<<<<<< HEAD
-      <TeaCeremony>{/* <Puck></Puck> */}</TeaCeremony>
-=======
       <AppContainer></AppContainer>
->>>>>>> et-operation
     </StrictMode>
   );
 }
